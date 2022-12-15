d937aaea438477e37d9a9f20f38c43d8ed206114
Joohwan Lee <aperire0402@gmail.com>
2022-12-15 04:47:02 -0500

    use np

--- src/openbrowser.py
+++ src/openbrowser.py
@@ -1,47 +1,88 @@
 import random
 import numpy as np
 from PIL import Image
+import requests
+from hashlib import sha256
 
 class Client:
-    def __init__(self):
-        pass
+    def __init__(self, connection: str):
+        self.connection = connection
+
+    def get_available_rpcs(self):
+        rpc_array = requests.get(self.connection).json
+        return rpc_array
+
 
     def get_enc_key(
         self,
-        pixel_n # number of pixels
+        pixel_n: int
     ):
+        """ 
+        Returns 1D array of encryption key
+        
+        Args:
+            pixel_n: Number of pixels in image
+
+        Example: 
+            >>> from openbrowser import Client
+            >>> client = Client()
+            >>> enc_key = client.get_enc_key(6016*6016)
+        """
         # create a random array to encrypt original file
         enc_key = [[random.randint(0,256) for j in range(3)] for i in range(pixel_n)]
         return enc_key
 
     def process_img_to_block(
         self,
-        img_path, # path to image
-        enc_key, # encryption key
-        block_size # size of pixels in a single block
+        img_path: str,
+        enc_key: list,
+        rpc_array: list
     ):
+        """ 
+        Returns private key and 2D array of blocks
+        
+        Args:
+            img_path: path to image file
+            enc_key: encryption key generated from client.get_enc_key(pixel_n)
+            rpc_array: list of selected RPCs to transfer data
+            
+        Example: 
+            >>> from openbrowser import Client
+            >>> client = Client()
+            >>> enc_key = client.get_enc_key(6016*6016)
+        """
+        # open image and get rgb array
         img = Image.open(img_path)
         raw_rgb_array = np.array(img)
+        shape = raw_rgb_array.shape
+        
+        # change shape to linear rgb: .reshape(1, y*x, 3: rgb)
+        rgb_array = raw_rgb_array.reshape(1, shape[0]*shape[1], shape[2])[0]
 
-        # Convert rgb array to 1D array
-        rgb_array = []
-        cnt = 0
-        for i in range(len(raw_rgb_array)):
-            for j in range(len(raw_rgb_array[i])):
-                pix = raw_rgb_array[i][j]
-                encryption = enc_key[cnt]
-                enc_pix = [i+j for i, j in zip(pix, encryption)]
-                rgb_array.append(enc_pix)
-                print(f"{100*cnt/(raw_rgb_array.shape[0]*raw_rgb_array.shape[1])}%")
-                cnt+=1
+        # encrypt each pixels
+        enc_rgb_array = np.add(rgb_array, enc_key)
+
+        # distribute pixels for each RPCs
+        enc_rgb_array = np.array_split(enc_rgb_array, len(rpc_array))
+        range_array = [len(i) for i in enc_rgb_array]
+
+        # get private key
+        private_key = {
+            "dim": (shape[0], shape[1]),
+            "enc": enc_key,
+            "rng": range_array,
+            "rpc": rpc_array
+        }
+
+        # get public key
+        public_key = sha256(str(private_key).encode('utf-8')).hexdigest()
+        print(private_key)
 
 
 
-client = Client()
-enc_key = client.get_enc_key(6016**2)
-print(client.process_img_to_block(
-    "img.jpeg",
-    enc_key,
-    0
-    )
-)
+
+        
+            
+
+
+
---
+++ src/test.py
@@ -0,0 +1,17 @@
+from openbrowser import Client
+
+
+client = Client("")
+enc_key = client.get_enc_key(200*200)
+
+
+rpc_array = ["gr", "ge", "e"]
+client.process_img_to_block(
+    "img.png",
+    enc_key,
+    rpc_array
+)
+
+'''
+200, 200, 3
+'''
